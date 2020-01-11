dec161ac58257e4ae35576606599d1d54caf39e3
benjamin solorzano <benjaenrique_@hotmail.com>
2020-01-11 10:56:02 -0600

    Control y vista de cerdas

---
+++ GranjaSystem/GranjaSystem/Controllers/CerdasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GranjaSystem.Models;
+
+namespace GranjaSystem.Controllers
+{
+    public class CerdasController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Cerdas
+        public ActionResult Index()
+        {
+            var cerdas = db.Cerdas.Include(t => t.Genetica);
+            return View(cerdas.ToList());
+        }
+
+        // GET: Cerdas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCerdas tblCerdas = db.Cerdas.Find(id);
+            if (tblCerdas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCerdas);
+        }
+
+        // GET: Cerdas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdGenetica = new SelectList(db.Geneticas, "IdGenetica", "Genetica");
+            return View();
+        }
+
+        // POST: Cerdas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCerdas,NumCerda,Procedencia,Observaciones,NumParto,FechaRegistro,Estado,IdGenetica")] tblCerdas tblCerdas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cerdas.Add(tblCerdas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdGenetica = new SelectList(db.Geneticas, "IdGenetica", "Genetica", tblCerdas.IdGenetica);
+            return View(tblCerdas);
+        }
+
+        // GET: Cerdas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCerdas tblCerdas = db.Cerdas.Find(id);
+            if (tblCerdas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdGenetica = new SelectList(db.Geneticas, "IdGenetica", "Genetica", tblCerdas.IdGenetica);
+            return View(tblCerdas);
+        }
+
+        // POST: Cerdas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCerdas,NumCerda,Procedencia,Observaciones,NumParto,FechaRegistro,Estado,IdGenetica")] tblCerdas tblCerdas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblCerdas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdGenetica = new SelectList(db.Geneticas, "IdGenetica", "Genetica", tblCerdas.IdGenetica);
+            return View(tblCerdas);
+        }
+
+        // GET: Cerdas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCerdas tblCerdas = db.Cerdas.Find(id);
+            if (tblCerdas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCerdas);
+        }
+
+        // POST: Cerdas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblCerdas tblCerdas = db.Cerdas.Find(id);
+            db.Cerdas.Remove(tblCerdas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
