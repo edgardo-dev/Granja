a4196e57d8042e51d39e745afaa122fae89d6eca
Fidel Garcia <edgardomartinez95@hotmail.com>
2020-02-07 09:26:35 -0600

    mejoras finales

---
+++ GranjaSystem/GranjaSystem/Migrations/202002071515407_vacuna.cs
@@ -0,0 +1,18 @@
+﻿namespace GranjaSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class vacuna : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tblDetalleLotes", "Fvacuna20", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tblDetalleLotes", "Fvacuna20");
+        }
+    }
+}
