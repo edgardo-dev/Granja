7b42e1e7b2da8aead8556aaf730cf9c0908f6ce5
Fidel Garcia <edgardomartinez95@hotmail.com>
2020-05-16 20:26:07 -0600

    Usuarios y Claves

---
+++ GranjaSystem/GranjaSystem/Migrations/202004240456189_qqComidas.cs
@@ -0,0 +1,20 @@
+﻿namespace GranjaSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class qqComidas : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tblLechones", "qqDiarios", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.tblLechones", "qqSemanal", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tblLechones", "qqSemanal");
+            DropColumn("dbo.tblLechones", "qqDiarios");
+        }
+    }
+}
